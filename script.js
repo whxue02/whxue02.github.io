e5a34cf23faf6cd59404e1b331a5067b0626d912
Cheryl Wang <whxcollege@gmail.com>
2024-07-21 02:00:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.getElementById('moreDetailsButton').addEventListener('click', function() {
-    const moreDetails = document.getElementById('moreDetails');
-    if (moreDetails.style.display === 'none') {
-        moreDetails.style.display = 'block';
-        this.textContent = 'Show Less Details';
-    } else {
-        moreDetails.style.display = 'none';
-        this.textContent = 'Show More Details';
-    }
-});
+
